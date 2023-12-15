07647d573cfda1eddc6f7d59c98890e98cff78c3
Fergus McNab <mcnab@gfz-potsdam.de>
2023-12-15 14:15:46 +0100

    glic submission script for variable segment length run.

---
+++ network/glic/submit_seg_length.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=10
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2-00:0
+
+module load Python
+source /home/mcnab/network/bin/activate
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 0 49 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 50 99 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 100 149 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 150 199 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 200 249 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 250 299 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 300 349 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 350 399 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 400 449 &
+srun --nodes=1 --ntasks=1 --cpus-per-task=10 --time=2-00:0 python network_sweep_seg_length.py 450 499 &
+wait
+deactivate
