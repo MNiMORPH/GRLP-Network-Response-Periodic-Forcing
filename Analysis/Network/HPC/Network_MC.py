a2e5b0989c16e8c2a149b7afa5072b54351ac31c
Fergus McNab <mcnab@gfz-potsdam.de>
2024-08-22 11:18:08 +0200

    Revert to 10 processes in parallel sweep.

--- Analysis/Network/HPC/Network_MC.py
+++ Analysis/Network/HPC/Network_MC.py
@@ -347,7 +347,7 @@ if __name__ == "__main__":
     print("Starting: " + datetime.now().strftime("%H:%M:%S"))
     
     import multiprocessing as mp
-    with mp.Pool(processes=3) as pool:
+    with mp.Pool(processes=10) as pool:
         results = pool.map(
             analyse_network, 
             [(k, setup_file) for k in range(i,j+1)]
