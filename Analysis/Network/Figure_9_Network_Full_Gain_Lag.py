f26097896eb0af42a254a525ccb1129f9bd38e47
Fergus McNab <mcnab@gfz-potsdam.de>
2024-09-09 15:04:29 +0200

    Tidy up and restructure plotting of full network gain and lag results.

--- Analysis/Network/Figure_9_Network_Full_Gain_Lag.py
+++ Analysis/Network/Figure_9_Network_Full_Gain_Lag.py
@@ -1,5 +1,5 @@
 """
-This script performs the analysis presented in Figure 8 of McNab et al. (2024,
+This script performs the analysis presented in Figure 9 of McNab et al. (2024,
 EGUsphere); produces a rough version of the Figure; and, optionally, generates
 output files for plotting the final Figure in GMT.
 
@@ -99,7 +99,7 @@ print("Plotting.")
 fig, axs = plt.subplots(6, 4, sharey="row", sharex=True)
 
 # Loop over network cases.
-for i,case in enumerate(nets[0].keys()):
+for i,case in enumerate(['UUU', 'NUU', 'UAU', 'NAU']):
     
     Teqs = [gs[case]['Teq'] for gs in gains]
     
@@ -310,11 +310,11 @@ for row in axs:
 plt.show()
 
 
+# ---- Save
+
 if output_gmt:
 
-    # ---- Save
-
-    basedir = "../output/network/full_sweep/"
+    basedir = "../../Output/Network/Figure_9_Network_Full_Gain_Lag/"
 
     with open(basedir + "z_linear_gain.pg", "wb") as f:
         arr = np.column_stack(( lin_periods/lp.equilibration_time, lin_gain_z ))
@@ -328,11 +328,21 @@ if output_gmt:
         np.savetxt(f, arr)
         
     with open(basedir + "z_continuous_gain.pg", "wb") as f:
-        gain1 = [max(np.hstack((p1['G_z'][0], p2['G_z'][0]))) for p1,p2 in zip(continuous_periodics[1.4], continuous_periodics[2.2])]
-        gain2 = [min(np.hstack((p1['G_z'][0], p2['G_z'][0]))) for p1,p2 in zip(continuous_periodics[1.4], continuous_periodics[2.2])]
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( gain1, gain2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['G_z'][0].min() for p in continuous_periodics[1.4]],
+                    [p['G_z'][0].min() for p in continuous_periodics[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['G_z'][0].max() for p in continuous_periodics[1.4]],
+                    [p['G_z'][0].max() for p in continuous_periodics[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)
         
@@ -348,24 +358,46 @@ if output_gmt:
         np.savetxt(f, arr)        
 
     with open(basedir + "z_continuous_lag.pl", "wb") as f:
-        lag1 = [max(np.hstack((p1['lag_z'][0], p2['lag_z'][0]))) for p1,p2 in zip(continuous_periodics[1.4], continuous_periodics[2.2])]
-        lag2 = [min(np.hstack((p1['lag_z'][0], p2['lag_z'][0]))) for p1,p2 in zip(continuous_periodics[1.4], continuous_periodics[2.2])]
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( lag1, lag2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['lag_z'][0].min() for p in continuous_periodics[1.4]],
+                    [p['lag_z'][0].min() for p in continuous_periodics[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['lag_z'][0].max() for p in continuous_periodics[1.4]],
+                    [p['lag_z'][0].max() for p in continuous_periodics[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)
 
     with open(basedir + "Qs_linear_gain.pg", "wb") as f:
-        arr = np.column_stack(( lin_periods/lp.equilibration_time, lin_gain_Qs ))
+        arr = np.column_stack((
+            lin_periods/lp.equilibration_time,
+            lin_gain_Qs ))
         np.savetxt(f, arr)
 
     with open(basedir + "Qs_continuous_gain.pg", "wb") as f:
-        gain1 = [p['G_Qs'][0][-1] for p in continuous_periodics[1.4]]
-        gain2 = [p['G_Qs'][0][-1] for p in continuous_periodics[2.2]]
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( gain1, gain2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['G_Qs'][0][-1].min() for p in continuous_periodics[1.4]],
+                    [p['G_Qs'][0][-1].min() for p in continuous_periodics[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['G_Qs'][0][-1].max() for p in continuous_periodics[1.4]],
+                    [p['G_Qs'][0][-1].max() for p in continuous_periodics[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)
         
@@ -373,111 +405,124 @@ if output_gmt:
         arr = np.column_stack(( lin_periods/lp.equilibration_time, lin_lag_Qs ))
         np.savetxt(f, arr)
 
-    # nonnans_a = np.where(~np.isnan(continuous_Qs_lags[0.5]['Qs']))
-    # nonnans_b = np.where(~np.isnan(continuous_Qs_lags[1]['Qs']))
-    # with open(basedir + "Qs_continuous_lag.pl", "wb") as f:
-    #     arr = np.column_stack(( 
-    #         np.hstack(( continuous_periods[nonnans_a], continuous_periods[nonnans_b][::-1] ))/lp.equilibration_time,
-    #         np.hstack(( np.array(continuous_Qs_lags[0.5]['Qs'])[nonnans_a], np.array(continuous_Qs_lags[1]['Qs'])[nonnans_b][::-1] ))
-    #         ))
-    #     np.savetxt(f, arr)
     with open(basedir + "Qs_continuous_lag.pl", "wb") as f:
-        lag1 = np.array([p['lag_Qs'] for p in continuous_periodics[1.4]])/continuous_periods
-        lag2 = np.array([p['lag_Qs'] for p in continuous_periodics[2.2]])/continuous_periods
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( lag1, lag2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['lag_Qs'] for p in continuous_periodics[1.4]],
+                    [p['lag_Qs'] for p in continuous_periodics[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['lag_Qs'] for p in continuous_periodics[1.4]],
+                    [p['lag_Qs'] for p in continuous_periodics[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)
 
-
     with open(basedir + "Qs_Qw_linear_gain.pg", "wb") as f:
-        arr = np.column_stack(( lin_periods/lp.equilibration_time, lin_gain_Qs_Qw ))
+        arr = np.column_stack((
+            lin_periods/lp.equilibration_time,
+            lin_gain_Qs_Qw
+            ))
         np.savetxt(f, arr)
         
     with open(basedir + "Qs_Qw_continuous_gain.pg", "wb") as f:
-        gain1 = [p['G_Qs'][0][-1] for p in continuous_periodics_Q[1.4]]
-        gain2 = [p['G_Qs'][0][-1] for p in continuous_periodics_Q[2.2]]
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( gain1, gain2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['G_Qs'][0][-1] for p in continuous_periodics_Q[1.4]],
+                    [p['G_Qs'][0][-1] for p in continuous_periodics_Q[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['G_Qs'][0][-1] for p in continuous_periodics_Q[1.4]],
+                    [p['G_Qs'][0][-1] for p in continuous_periodics_Q[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)
 
     with open(basedir + "Qs_Qw_linear_lag.pl", "wb") as f:
-        arr = np.column_stack(( lin_periods/lp.equilibration_time, lin_lag_Qs_Qw ))
+        arr = np.column_stack((
+            lin_periods/lp.equilibration_time,
+            lin_lag_Qs_Qw
+            ))
         np.savetxt(f, arr)
 
-    # nonnans_a = np.where(~np.isnan(continuous_Qs_lags[0.5]['Qs']))
-    # nonnans_b = np.where(~np.isnan(continuous_Qs_lags[1]['Qs']))
-    # with open(basedir + "Qs_Qw_continuous_lag.pl", "wb") as f:
-    #     arr = np.column_stack(( 
-    #         np.hstack(( continuous_periods[nonnans_a], continuous_periods[nonnans_b][::-1] ))/lp.equilibration_time,
-    #         np.hstack(( np.array(continuous_Qs_lags[0.5]['Qw'])[nonnans_a], np.array(continuous_Qs_lags[1]['Qw'])[nonnans_b][::-1] ))
-    #         ))
-    #     np.savetxt(f, arr)
     with open(basedir + "Qs_Qw_continuous_lag.pl", "wb") as f:
-        lag1 = np.array([p['lag_Qs'] for p in continuous_periodics_Q[1.4]])/continuous_periods
-        lag2 = np.array([p['lag_Qs'] for p in continuous_periodics_Q[2.2]])/continuous_periods
         arr = np.column_stack(( 
-            np.hstack(( continuous_periods, continuous_periods[::-1] ))/lp.equilibration_time,
-            np.hstack(( lag1, lag2[::-1] ))
+            np.hstack((
+                continuous_periods,
+                continuous_periods[::-1]
+                ))/lp.equilibration_time,
+            np.hstack((
+                np.column_stack((
+                    [p['lag_Qs'] for p in continuous_periodics_Q[1.4]],
+                    [p['lag_Qs'] for p in continuous_periodics_Q[2.2]]
+                    )).min(axis=1),
+                np.column_stack((
+                    [p['lag_Qs'] for p in continuous_periodics_Q[1.4]],
+                    [p['lag_Qs'] for p in continuous_periodics_Q[2.2]]
+                    )).max(axis=1)[::-1],    
+                ))
             ))
         np.savetxt(f, arr)        
 
 
-    outdirs = {
-        "m40_no_int": "m40_fix_seg_length_no_internal/",
-        "m40_w_int": "m40_fix_seg_length_w_internal/",
-        "m40_rnd_no_int": "m40_rnd_seg_length_no_internal/",
-        "m40_rnd_w_int": "m40_rnd_seg_length_w_internal/",
-        "m40_no_int_var_width": "m40_fix_seg_length_no_internal_var_width/",
-        "m40_w_int_var_width": "m40_fix_seg_length_w_internal_var_width/",
-        "m40_rnd_no_int_var_width": "m40_rnd_seg_length_no_internal_var_width/",
-        "m40_rnd_w_int_var_width": "m40_rnd_seg_length_w_internal_var_width/"
-        }
-
-    for i,sweep in enumerate(indirs.keys()):
+    for case in ['UUU', 'NUU', 'UAU', 'NAU']:
         
-        sweep_periods = np.hstack([g['P']/g['Teq_Qs'] for g in gains[sweep]])
-        sweep_mags = np.hstack([np.full(len(gains[sweep][j]['P']), len(n.streams_by_order[1])) for j,n in enumerate(nets[sweep])])
-
-        sweep_gains = [g['G_z']['Qs'][j][0][-1] for g in gains[sweep] for j in range(len(g['P']))]
-        sweep_gains_plus = [np.nanmax(np.hstack(g['G_z']['Qs'][j]))-g['G_z']['Qs'][j][0][-1] for g in gains[sweep] for j in range(len(g['P']))]
-        sweep_gains_minus = [g['G_z']['Qs'][j][0][-1]-np.nanmin(np.hstack(g['G_z']['Qs'][j])) for g in gains[sweep] for j in range(len(g['P']))]
-        with open(basedir + outdirs[sweep] + "z_gain.pg", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_gains, sweep_mags, sweep_gains_minus, sweep_gains_plus ))
+        Teqs = [gs[case]['Teq'] for gs in gains]
+        
+        with open(basedir + case + "/z_gain.pg", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,gs in enumerate(gains) for p in gs[case]['P']],
+                [g[0][-1] for gs in gains for g in gs[case]['G_z']['Qs']],
+                [g[0][-1]-np.hstack(g).min() for gs in gains for g in gs[case]['G_z']['Qs']],
+                [np.hstack(g).max()-g[0][-1] for gs in gains for g in gs[case]['G_z']['Qs']]
+                ))
+            np.savetxt(f, arr)
+        
+        with open(basedir + case + "/z_lag.pl", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,gs in enumerate(lags) for p in gs[case]['P']],
+                [l[0][-1] for ls in lags for l in ls[case]['lag_z']['Qs']],
+                [l[0][-1]-np.hstack(l).min() for ls in lags for l in ls[case]['lag_z']['Qs']],
+                [np.hstack(l).max()-l[0][-1] for ls in lags for l in ls[case]['lag_z']['Qs']]
+                ))
             np.savetxt(f, arr)
             
-        sweep_lags = [l['lag_z']['Qs'][j][0][-1] for l in lags[sweep] for j in range(len(l['P']))]
-        sweep_lags_plus = [np.nanmin(np.hstack(l['lag_z']['Qs'][j]))-l['lag_z']['Qs'][j][0][-1] for l in lags[sweep] for j in range(len(l['P']))]
-        sweep_lags_minus = [l['lag_z']['Qs'][j][0][-1]-np.nanmin(np.hstack(l['lag_z']['Qs'][j])) for l in lags[sweep] for j in range(len(l['P']))]
-        with open(basedir + outdirs[sweep] + "z_lag.pl", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_lags, sweep_mags, sweep_lags_minus, sweep_lags_plus ))
+        with open(basedir + case + "/Qs_gain.pg", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,gs in enumerate(gains) for p in gs[case]['P']],
+                [g[0][-1] for gs in gains for g in gs[case]['G_Qs']['Qs']]
+                ))
             np.savetxt(f, arr)
             
-        sweep_gains = [g['G_Qs']['Qs'][j][0][-1] for g in gains[sweep] for j in range(len(g['P']))]
-        with open(basedir + outdirs[sweep] + "Qs_gain.pg", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_gains, sweep_mags ))
+        with open(basedir + case + "/Qs_lag.pl", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,ls in enumerate(lags) for p in ls[case]['P']],
+                [l for ls in lags for l in ls[case]['lag_Qs']['Qs']]
+                ))
             np.savetxt(f, arr)
             
-        if i>3:
-            sweep_lags = [l['lag_Qs']['Qs'][j]/l['P'][j] for l in lags[sweep] for j in range(len(l['P']))]
-        else:
-            sweep_lags = [l['lag_Qs']['Qs'][j] for l in lags[sweep] for j in range(len(l['P']))]
-        with open(basedir + outdirs[sweep] + "Qs_lag.pl", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_lags, sweep_mags ))
+        with open(basedir + case + "/Qs_Qw_gain.pg", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,gs in enumerate(gains) for p in gs[case]['P']],
+                [g[0][-1] for gs in gains for g in gs[case]['G_Qs']['Qw']]
+                ))
             np.savetxt(f, arr)
             
-        sweep_gains = [g['G_Qs']['Qw'][j][0][-1] for g in gains[sweep] for j in range(len(g['P']))]
-        with open(basedir + outdirs[sweep] + "Qs_Qw_gain.pg", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_gains, sweep_mags ))
-            np.savetxt(f, arr)
-
-        if i>3:
-            sweep_lags = [l['lag_Qs']['Qw'][j]/l['P'][j] for l in lags[sweep] for j in range(len(l['P']))]
-        else:
-            sweep_lags = [l['lag_Qs']['Qw'][j] for l in lags[sweep] for j in range(len(l['P']))]
-        with open(basedir + outdirs[sweep] + "Qs_Qw_lag.pl", "wb") as f:
-            arr = np.column_stack(( sweep_periods, sweep_lags, sweep_mags ))
+        with open(basedir + case + "/Qs_Qw_lag.pl", "wb") as f:
+            arr = np.column_stack((
+                [p/Teqs[i] for i,ls in enumerate(lags) for p in ls[case]['P']],
+                [l for ls in lags for l in ls[case]['lag_Qs']['Qw']]
+                ))
             np.savetxt(f, arr)
--- Plotting/Network/Figure_9_Network_Full_Gain_Lag.sh
+++ Plotting/Network/Figure_9_Network_Full_Gain_Lag.sh
@@ -9,8 +9,8 @@ gmt_extras::set_gmt_defaults
 # variables
 out=sweep
 proj=-JX1.5il/1.5i
-basedir="../../output/network/full_sweep"
-sweepdirs=("m40_fix_seg_length_no_internal" "m40_rnd_seg_length_no_internal" "m40_fix_seg_length_w_internal" "m40_rnd_seg_length_w_internal")
+basedir="../../Output/Network/Figure_9_Network_Full_Gain_Lag"
+sweepdirs=("UUU" "NUU" "UAU" "NAU")
 G_z_labels=("a" "b" "c" "d")
 lag_z_labels=("e" "f" "g" "h")
 G_Qs_labels=("i" "j" "k" "l")
@@ -38,11 +38,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psxy $basedir/z_continuous_gain.pg $rgn $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/z_linear_gain_rng.pg $rgn $proj -Gdimgrey -t70 -O -K >> $out.ps
   gmt psxy $basedir/z_linear_gain.pg $rgn $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/z_gain.pg $rgn $proj -Sc3p -W0.8p+cl -Cmag.cpt -Ey+a+p0.8p+w2p+cl -t95 -O -K >> $out.ps
-  # awk ' { print $1, $2, $4, $5 } ' $basedir/${sweepdirs[$i]}/z_gain.pg | \
-  #   gmt psxy $rgn $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t92 -O -K >> $out.ps
-  awk ' { print $1, $2, $4, $5 } ' $basedir/${sweepdirs[$i]}_var_width/z_gain.pg | \
-    gmt psxy $rgn $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/z_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t95 -O -K >> $out.ps
   echo "${G_z_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jTL+cTL -D0.05i/-0.08i -O -K >> $out.ps
   gmt psbasemap $rgn $proj -Bnse${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.2+l"Gain, @%2%G@-z@-@%% [-]" -O -K >> $out.ps
   echo "${titles[$i]}" | gmt pstext $rgn $proj -F+f8p+jCB+cCT -D0i/0.1i -N -O -K >> $out.ps
@@ -52,11 +48,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psxy $basedir/z_continuous_lag.pl $rgn_l $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/z_linear_lag_rng.pl $rgn_l $proj -Gdimgrey -t70 -O -K >> $out.ps
   gmt psxy $basedir/z_linear_lag.pl $rgn $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/z_lag.pl $rgn_l $proj -Sc3p -W0.8p+cl -Cmag.cpt -Ey+a+p0.8p+w2p+cl -t95 -O -K >> $out.ps
-  # awk ' { print $1, $2, $4, $5 } ' $basedir/${sweepdirs[$i]}/z_lag.pl | \
-  #   gmt psxy $rgn $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t92 -O -K >> $out.ps
-  awk ' { print $1, $2, $4, $5 } ' $basedir/${sweepdirs[$i]}_var_width/z_lag.pl | \
-    gmt psxy $rgn $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/z_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -Ey+a+p0.8p,steelblue+w2p -t95 -O -K >> $out.ps
   echo "${lag_z_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jTL+cTL -D0.05i/-0.08i -O -K >> $out.ps
   gmt psbasemap $rgn_l $proj -Bnse${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.2+l"Lag, @~\152@~@%2%@-z@-@%% / @%2%P@%% [-]" -O -K >> $out.ps
   
@@ -64,8 +56,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psbasemap $rgn $proj -B+n -Y-1.65i -O -K >> $out.ps
   gmt psxy $basedir/Qs_continuous_gain.pg $rgn $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/Qs_linear_gain.pg $rgn $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/Qs_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
-  gmt psxy $basedir/${sweepdirs[$i]}_var_width/Qs_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/Qs_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
   echo "${G_Qs_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jTL+cTL -D0.05i/-0.08i -O -K >> $out.ps
   gmt psbasemap $rgn $proj -Bnse${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.2+l"Gain, @%2%G@-Qs,L@-@%% [-]" -O -K >> $out.ps
   
@@ -73,8 +64,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psbasemap $rgn_l $proj -B+n -Y-1.65i -O -K >> $out.ps
   gmt psxy $basedir/Qs_continuous_lag.pl $rgn_l $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/Qs_linear_lag.pl $rgn_l $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/Qs_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
-  gmt psxy $basedir/${sweepdirs[$i]}_var_width/Qs_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/Qs_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
   echo "${lag_Qs_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jTL+cTL -D0.05i/-0.08i -O -K >> $out.ps
   gmt psbasemap $rgn_l $proj -Bnse${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.2+l"Lag, @~\152@~@%2%@-Qs,L@-@%% / @%2%P@%% [-]" -O -K >> $out.ps
 
@@ -82,8 +72,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psbasemap $rgn $proj -B+n -Y-1.65i -O -K >> $out.ps
   gmt psxy $basedir/Qs_Qw_continuous_gain.pg $rgn $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/Qs_Qw_linear_gain.pg $rgn $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/Qs_Qw_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
-  gmt psxy $basedir/${sweepdirs[$i]}_var_width/Qs_Qw_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/Qs_Qw_gain.pg $rgn $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
   echo "${G_Qs_Qw_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jTL+cTL -D0.05i/-0.08i -O -K >> $out.ps
   gmt psbasemap $rgn $proj -Bnse${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.2+l"Gain, @%2%G@-Qs,L@-@%% [-]" -O -K >> $out.ps
 
@@ -91,8 +80,7 @@ for i in ${!sweepdirs[@]} ; do
   gmt psbasemap $rgn_l $proj -B+n -Y-1.65i -O -K >> $out.ps
   gmt psxy $basedir/Qs_Qw_continuous_lag.pl $rgn_l $proj -G197.12/64.125/125.88 -t70 -O -K >> $out.ps
   gmt psxy $basedir/Qs_Qw_linear_lag.pl $rgn_l $proj -W0.8p -O -K >> $out.ps
-  # gmt psxy $basedir/${sweepdirs[$i]}/Qs_Qw_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
-  gmt psxy $basedir/${sweepdirs[$i]}_var_width/Qs_Qw_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
+  gmt psxy $basedir/${sweepdirs[$i]}/Qs_Qw_lag.pl $rgn_l $proj -Sc3p -W0.8p,steelblue -t92 -O -K >> $out.ps
   echo "${lag_Qs_Qw_labels[$i]}" | gmt pstext $rgn $proj -F+f11p,Helvetica-Bold,black+jBL+cBL -D0.05i/0.08i -O -K >> $out.ps
   gmt psbasemap $rgn_l $proj -BnSe${W} -Bx1f3p+l"Period, @%2%P@%% / @%2%T@-eq@-@%% [-]" -By0.05+l"Lag, @~\152@~@%2%@-Qs,L@-@%% / @%2%P@%% [-]" -O -K >> $out.ps
 
