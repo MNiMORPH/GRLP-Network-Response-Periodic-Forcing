a6e31ca851d99b0098c5ca786d01ffef01f198c4
Fergus McNab <mcnab@gfz-potsdam.de>
2024-10-11 08:28:46 +0200

    Prepare for next sweep.

--- Analysis/Network/HPC/Submit_N1_2-102.sh
+++ Analysis/Network/HPC/Submit_N1_2-102.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes=5
+#SBATCH --nodes=10
 #SBATCH --ntasks=50
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --time=10-00:0
 
 # Load python
@@ -20,8 +20,8 @@ for N1_min in $(seq $min_N1_min 10 $max_N1_min) ; do
   for N1 in $(seq $N1_min 2 $(echo $N1_min | awk ' {print $1 + 8} ')) ; do
     
     for j in $(seq 0 1) ; do
-      srun --exclusive --nodes=1 --ntasks=1 --cpus-per-task=4 \
-        python Network_MC_2.py \
+      srun --exclusive --nodes=1 --ntasks=1 --cpus-per-task=8 \
+        python Network_MC.py \
         $i $N1 "/home/mcnab/grlp_network_analysis/Output/Network/MC_N1_2-102/" &
       if [ $i -eq 49 ] ; then
         wait
