5ff40d97df1a5a1cad8892ed3410b949debb2aed
Fergus McNab <mcnab@gfz-potsdam.de>
2024-12-02 10:46:07 +0100

    Prepare for sweep.

--- Analysis/Network/HPC/Submit_MC_N1_2-150.sh
+++ Analysis/Network/HPC/Submit_MC_N1_2-150.sh
@@ -24,7 +24,7 @@ for N1 in $(seq $N1_min 1 $N1_max) ; do
     # Submit the job
     srun --exclusive --nodes=1 --ntasks=1 --cpus-per-task=8 \
       python Network_MC.py \
-      $i $N1 "/home/mcnab/grlp_network_analysis/Output/Network/MC_N1_2-152/" &
+      $i $N1 "/home/mcnab/grlp_network_analysis/Output/Network/MC_N1_2-150/" &
     
     # Every 100th job, wait for jobs to finish before continuing
     if [ $i -eq 99 ] || [ $i -eq 199 ] || [ $i -eq 299 ] || [ $i -eq 399 ] || [ $i -eq 499 ] ; then
